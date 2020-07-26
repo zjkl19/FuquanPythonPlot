72fa235e80f1b201e312563a19f36c11503bdec1
迪南 林 <eamdfan@hotmail.com>
2020-07-26 22:43:43 +0800

    add the plot features

--- main.py
+++ main.py
@@ -43,14 +43,41 @@ base = datetime.datetime(2020, 7, 17)
 dates = [base + datetime.timedelta(hours=(1 * i)) for i in range(dataCounts)]    #1小时1个数据
 lims = [(np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
         (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
+        (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00')),
         (np.datetime64('2020-07-17'), np.datetime64('2020-07-24 00:00'))]
-ylims = [(-1.0, 1.0),
-        (-1.0, 1.0),
-        (-1.0, 1.0)]
-yList = [[] for i in range(nPoints)]  # 创建的是多行三列的二维列表
 
-fig, ax = plt.subplots(constrained_layout=True, figsize=(xFigsize, yFigsize))
 
+#F17-D-位移(mm)	G11-D-位移(mm)	F17-L-倾角(°)	G11-L-倾角(°)	D2-2倾角-倾角(°)	ZW24-1倾角-倾角(°)	ZW24-2倾角-倾角(°)	ZE24-1倾角-倾角(°)	ZE24-2倾角-倾角(°)	D2-2-1位移-位移(mm)	D2-2-2位移-位移(mm)	ZW24-1位移-位移(mm)	ZW24-2位移-位移(mm)	ZE24-1位移-位移(mm)	ZE24-2位移-位移(mm)
+
+
+defaultLeanLims=(-0.1,0.1)
+
+ylabelList=['位移(mm)','位移(mm)','倾角(°)','倾角(°)','倾角(°)','倾角(°)','倾角(°)','倾角(°)','倾角(°)','位移(mm)','位移(mm)','位移(mm)','位移(mm)','位移(mm)','位移(mm)']
+
+ylims = [(-1.0, 1.0),(-1.0, 1.0),defaultLeanLims,defaultLeanLims,defaultLeanLims
+        ,defaultLeanLims,defaultLeanLims,defaultLeanLims,defaultLeanLims,(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-2.0, 2.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)]
+
+alerLine = [(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-2.0, 2.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)
+        ,(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0),(-1.0, 1.0)]
+
+yList = [[] for i in range(nPoints)]  # 创建的是多行n列的二维列表
 for i in range(0,nPoints):
     startRow=2
     temp=[]    
@@ -58,12 +85,21 @@ for i in range(0,nPoints):
         temp.append(sh.cell_value(startRow+j,i+1))
     yList[i] = np.array(temp)
 
-ax.plot(dates, yList[0])
-ax.grid()
-ax.set_xlim(lims[0])
-ax.set_ylim(ylims[0])
-
-plt.savefig(measurePointsList[0]+'.jpg')
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+   
+for i in range(0,nPoints):
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(xFigsize, yFigsize))
+    
+    ax.plot(dates, yList[i])
+    ax.set(xlabel='时间', ylabel=ylabelList[i])
+    
+    ax.grid()
+    ax.set_xlim(lims[i])
+    ax.set_ylim(ylims[i])
+    
+    #plt.axhline(y=0.25,c="yellow")#添加水平直线
+    plt.savefig(measurePointsList[i]+'.jpg')
 #k=0
 #for nn, ax in enumerate(axs):
 #          
